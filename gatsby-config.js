d5a6294b25dab8d84aeb2ae80c64c246f86c6eda
m_kawashima <m_kawashima@lyzon.co.jp>
2020-12-28 15:14:47 +0900

    change GAID

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,9 +49,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "UA-117563332-3", // Google Analytics
+          "G-RQJWQXMRJN", // Google Analytics
         ],
       },
     }
