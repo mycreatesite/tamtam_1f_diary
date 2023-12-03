7ffd71982d9cf00e3245feeee8ab4e0b5c1aaf35
ma-ya's create <hello.myscreate@gmail.com>
2023-12-03 12:57:49 +0900

    fix: scss, ogp, etc

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,13 +1,9 @@
 import React from 'react'
-import BgHero from "../../static/img/bg-hero.svg";
 import Hero from "../../static/img/hero.svg";
 import './scss/hero.scss'
 
 export default (props) => (
 	<div className='hero'>
-		<div className='bgHero'>
-			<BgHero />
-		</div>
 		<h1 className='heroImage'>
 			<Hero />
 		</h1>
